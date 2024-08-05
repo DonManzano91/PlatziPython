ba40b6ca5d9b77475216cc17aa4af48c4c5af022
DonManzano91 <alex.batt99@gmail.com>
2024-08-04 19:22:09 -0600

    main first commit

---
+++ charts/charts.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as pylot
+
+def generar_grafica_pie():
+    labels=['A', 'B', 'C']
+    values = [200, 100, 20]
+
+    fig, ax = pylot.subplots()
+    ax.pie(values, labels=labels)
+    pylot.savefig('pie.png')
+    pylot.close()
+    print("Funcion generar_grafica_pie() ejecutada correctamente")
+    
---
+++ charts/main.py
@@ -0,0 +1,7 @@
+import charts
+
+def run():
+    charts.generar_grafica_pie()
+
+if __name__ == '__main__':
+    run()
