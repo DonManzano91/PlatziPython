8f87f371123df90dffea816f088a276d6ebfc8ee
DonManzano91 <alex.batt99@gmail.com>
2025-01-01 22:54:32 -0600

    adicion html directo en response

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,6 @@
 import store
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -7,10 +8,18 @@ app = FastAPI()
 def get_list():
     return[1,2,3]
 
-@app.get("/nombre")
+@app.get("/nombre", response_class=HTMLResponse)
 def get_nombre():
-    return{"nombre" : "alejandro"}
-
+    return '''
+    <html>
+        <head>
+            <title>Respuesta en HTML</title>
+        </head>
+        <body>
+            <h1>Asi es, respuesta en html</h1>
+        </body>
+    </html>
+    '''
 def run():
     store.get_categorias()
 
